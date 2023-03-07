92a07057db8b5293cd6d1c9ee08f75a2c4b81384
JuanBosisio <juancruzbosisio2@hotmail.com>
2023-03-07 17:30:34 -0300

    Antes de finalizar

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card({updateName},{updateColor}) {
+function Card({updateName,updateColor}) {
 
 
 
@@ -9,7 +9,7 @@ function Card({updateName},{updateColor}) {
     <div>
       <h2>Hola {updateName}</h2>
       <h3>Sabemos que tu color favorito es:</h3>
-      {updateColor}
+      <h3>{updateColor}</h3>
       
     </div>
   );
